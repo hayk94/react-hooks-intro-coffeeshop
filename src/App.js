5d7ca550e242578a55a75f777e0e777502e9bacd
Hayk <hayk94@gmail.com>
2019-03-22 17:36:03 +0400

    implemented MenuFC with useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import Menu from './Menu';
+import MenuFc from './MenuFC';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <section>
+          <h6>
+            Class
+          </h6>
           <Menu/>
+        </section>
+        <section>
+          <h6>
+            Hooks
+          </h6>
+          <MenuFc/>
         </section>
       </div>
     );
---
+++ src/MenuFC.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+
+const MenuFc = () => {
+  const [selected, setSelected] = useState('Purple Haze');
+  const onProductChange = (e) => {
+    setSelected(e.target.value);
+  };
+
+  const [count, setCount] = useState(0);
+  const onCountChange = (e) => {
+    setCount(e.target.value);
+  };
+
+  const onOrder = () => {
+    setTimeout(() => {
+      alert(`You ordered ${count} ${selected}`);
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <div>
+        <b>Product: </b>
+        <select onChange={onProductChange}>
+          <option value="Purple Haze">Purple Haze</option>
+          <option value="Amnesia">Amnesia</option>
+          <option value="GoGreen">GoGreen</option>
+        </select>
+      </div>
+      <div>
+        <b>Count: </b>
+        <input
+          type="number"
+          min={0}
+          value={count}
+          onChange={onCountChange}
+        />
+      </div>
+      <div>
+        <button onClick={onOrder}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default MenuFc;
